3169f2e141dab62c833d20d1ed6154b0377953e7
Seokmin Lee <leesk212@gmail.com>
2024-06-08 19:36:01 +0900

    start
    timestamp

---
+++ 2573/소스.cpp
@@ -0,0 +1,30 @@
+/*
+- Type : BFS(추정)
+- INDEX : BJ-GOLD4
+- NAME : 빙산
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+#include <queue>
+#include <algorithm>
+#include <string.h>
+
+using namespace std;
+
+
+int main(void) {
+	freopen("텍스트.txt", "r", stdin);
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+
+
+	return 0;
+
+}
