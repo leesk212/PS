d0e50045d9893f7171234feef42f34ff7b55748e
Seokmin Lee <leesk212@gmail.com>
2024-06-08 17:42:26 +0900

    BFS 로 시간초과 해결
    0층이면 오류!
    
    BFS와 DFS의 차이를 정리하자

--- 5014/소스.cpp
+++ 5014/소스.cpp
@@ -2,13 +2,14 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 int F, S, G, U, D;
 int moving_dir[2] = { 0, };
 int visted_floor[1000001] = { 0, };
-int ans = 0;
+int distance_[1000001] = { 0, }; // 해당 위치로 가는데 까지 걸리는 시간 (해당문제의 Key Point)
 
 void init() {
 	cin >> F // 전체 층수 
@@ -19,27 +20,54 @@ void init() {
 }
 
 
-void dfs(int cnt,int cur) {
+//void dfs(int cnt,int cur) {
+//	
+//	if (cur == G) {
+//		ans = cnt;
+//		cout << ans;
+//		exit(1);
+//	}
+//
+//	for (int i = 0; i < 2; i++) {
+//		int next = cur + moving_dir[i];
+//		
+//		//혹시라도 층수를 넘겼을 경우! 그냥 넘어가자!
+//		if (next > F || next < 0) continue; 
+//		
+//		//해당 층수를 방문했으면 굳이 또 방문해야할까? ㄴㄴ
+//		if (visted_floor[next] == 1) continue;
+//
+//		visted_floor[next] = 1;
+//		dfs(cnt + 1, next);
+//		visted_floor[next] = 0;
+//	}
+//
+//}
+
+void bfs(int cur) {
+	queue<int> q;
+	q.push(cur);
+	visted_floor[cur] = true;
+
+	while (!q.empty()) {
+		int current_floor = q.front();
+		q.pop();
+
+		for (int i = 0; i < 2; i++) {
+			int next_floor = current_floor + moving_dir[i];
+
+			if (next_floor<=0 || next_floor> F) continue;
+
+			if (visted_floor[next_floor] == 1) continue;
+
+			visted_floor[next_floor] = true;
+			q.push(next_floor);
+			distance_[next_floor] = distance_[current_floor] + 1; //이 문제의 keypoint 거리별로 증가되는 폭을 늘려주는 것!
+
+		}
+
+	}
 	
-	if (cur == G) {
-		ans = cnt;
-		cout << ans;
-		exit(1);
-	}
-
-	for (int i = 0; i < 2; i++) {
-		int next = cur + moving_dir[i];
-		
-		//혹시라도 층수를 넘겼을 경우! 그냥 넘어가자!
-		if (next > F || next < 0) continue; 
-		
-		//해당 층수를 방문했으면 굳이 또 방문해야할까? ㄴㄴ
-		if (visted_floor[next] == 1) continue;
-
-		visted_floor[next] = 1;
-		dfs(cnt + 1, next);
-		visted_floor[next] = 0;
-	}
 
 }
 
@@ -49,7 +77,8 @@ void game() {
 	moving_dir[1] = -1*D;
 	int current_pos = S;
 
-	dfs(0,current_pos);
+	//dfs(0,current_pos);
+	bfs(current_pos);
 }
 
 
@@ -58,10 +87,11 @@ int main() {
 
 	init();
 	game();
-
-	if (ans == 0) {
+	
+	if (visted_floor[G]) {
+		cout << distance_[G];
+	}
+	else {
 		cout << "use the stairs";
 	}
-
-
 }
