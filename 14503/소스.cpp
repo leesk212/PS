8dd477a2b27a58c20470726f9bf8fafd73a698b9
leesk212 <leesk212@gmail.com>
2024-06-28 21:42:35 +0900

    Let's PS
    Start

---
+++ 14503/소스.cpp
@@ -0,0 +1,20 @@
+#define USE_CRT_NO_SECURE
+
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	iostream::sync_with_stdio(0);
+
+	freopen("≈ÿΩ∫∆Æ.txt", "r", stdin);
+
+
+}
